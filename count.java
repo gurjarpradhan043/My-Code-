cefa9c079f345d757ff4daf648136c950e140b95
Pradhan Gurjar <124359288+gurjarpradhan043@users.noreply.github.com>
2024-11-30 14:07:45 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class count {
+    public static void main(String[] args) {
+        Scanner count = new Scanner(System.in);
+        System.out.print("Enter number for counting : ");
+      int  num = count.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.println( i );
+        }
+        
+    }
+}
