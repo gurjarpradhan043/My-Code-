efe07ba11bef00a09b74ddcb2efd70ed24556198
Pradhan Gurjar <gurjarpradhan043@gmail.com>
2024-12-11 21:37:19 +0530

    New Code

--- python/exp8.py
+++
@@ -1,8 +0,0 @@
-def max_of_three(a, b, c):
-    return max(a, b, c)
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
-
-print("The max is:", max_of_three(a, b, c))
--- python/randomNumber.py
+++
@@ -1,2 +0,0 @@
-import random
-for x in range(1,20): print(x)
