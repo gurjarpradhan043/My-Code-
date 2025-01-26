d9cb4f51329bdff81c68e7b9571e254e71db195a
Pradhan Gurjar <gurjarpradhan043@gmail.com>
2025-01-26 15:21:51 +0530

    Code updated

--- DSA/arrayList.java
+++ DSA/arrayList.java
@@ -1,7 +1,21 @@
 package DSA;
+import java.util.ArrayList;
 
 public class arrayList {
     public static void main(String[] args) {
-        System.out.println("Radhe Radhe ");
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        // ArrayList<String> secondList = new ArrayList<>();
+        // ArrayList<Boolean> thirdList = new ArrayList<>();
+
+        //Add elements 
+        l.add(0);
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+
+        System.out.println(l);
+
+
     }
 }
