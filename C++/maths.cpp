ac41b57c55bd0c4505fc3d5cb17e890ef986c34d
Pradhan Gurjar <gurjarpradhan043@gmail.com>
2025-02-15 20:58:37 +0530

    Code is Up to Date

---
+++ C++/maths.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+int main() {
+    int x = 10;
+    int z = ++x; // x=11,z=10
+    std::cout << z;
+
+    
+    return 0;
+}
---
+++ C++/try.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main() {
+    int temp;
+    int a = 5;
+    int b = 6;
+    std::cout << "before swaping a = %d and b = %d",a , b ;
+     temp = a;
+      a = b;
+      b = temp;
+    std::cout << "after swaping a = %d and b = %d ",a,b;
+    return 0;
+}
